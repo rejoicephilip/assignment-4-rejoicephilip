c5efb8f2cbe44d1579e2a19e0295b7c1fb5da8c5
rejoicephilip <rejoicephilip8@gmail.com>
2025-11-03 18:07:35 -0700

    sharedpreferences for user prefes

--- 1251-assignment-4-template-main/lib/managers/preferences_manager.dart
+++ 1251-assignment-4-template-main/lib/managers/preferences_manager.dart
@@ -5,31 +5,40 @@ class PreferencesManager {
 
   static const PreferencesManager instance = PreferencesManager._();
 
-  static const String userNameKey = 'userName';
-  static const String darkModeKey = 'darkMode';
+  static const String _keyuserName = 'username';
+  static const String _keydarkMode = 'darkmode';
+  static const String _keyIsFirstRun = 'is_first_run';
 
   Future<void> setUserName(String userName) async {
     final prefs = await SharedPreferences.getInstance();
-    await prefs.setString(userNameKey, userName);
+    await prefs.setString(_keyuserName, userName);
   }
 
   Future<String?> getUserName() async {
     final prefs = await SharedPreferences.getInstance();
-    return prefs.getString('user_Name');
+    return prefs.getString(_keyuserName);
   }
 
   Future<void> setDarkMode(bool darkMode) async {
     final prefs = await SharedPreferences.getInstance();
-    await prefs.setBool(darkModeKey, darkMode);
+    await prefs.setBool(_keydarkMode, darkMode);
   }
 
   Future<bool> getDarkMode() async {
     final prefs = await SharedPreferences.getInstance();
-    return prefs.getBool(darkModeKey) ?? false;
+    return prefs.getBool(_keydarkMode) ?? false;
   }
 
   Future<bool> isFirstRun() async {
-    final userName = await getUserName();
-    return userName == null;
+    final prefs = await SharedPreferences.getInstance();
+    final isFirst = prefs.getBool(_keyIsFirstRun) ?? true;
+     if (isFirst) {
+      await prefs.setBool(_keyIsFirstRun, false);
+    }
+    return isFirst;   
+ }
+  Future<void> resetPreferences() async{
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.clear();
   }
 }
